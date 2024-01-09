4eeff0de03427758dc53bfcadcb297c3f3c38b10
Joseph Chang <k7451797@gmail.com>
2024-01-09 10:08:33 +0800

    ci: add prettier

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx,cjs}': [
-    'eslint --cache --fix --report-unused-disable-directives --max-warnings=0',
+  "*.{js,jsx,ts,tsx,cjs}": [
+    "eslint --cache --fix --report-unused-disable-directives --max-warnings=0",
     // https://github.com/okonet/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
-    () => 'pnpm run checkTypes',
+    () => "pnpm run checkTypes",
   ],
-  '**': 'cspell lint --no-must-find-files',
+  "**": "cspell lint --no-must-find-files",
 };
